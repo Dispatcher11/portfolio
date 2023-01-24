bde3f25053f6df465c61c812aef346f2a5e0c7b3
belalelgebaly <belalelgebaly4@gmail.com>
2023-01-24 16:48:51 +0200

    navbar for mobile devices

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import React from "react";
 import Toggle from "./Toggle";
+import { TiPencil } from 'react-icons/ti';
 
 const Navbar = () => {
     return ( 
@@ -14,12 +15,32 @@ const Navbar = () => {
       // dragConstraints={{left: 0, top: 0, right: 1400, bottom: 550}}
       dragElastic={.7}>belal</motion.h1>
                 <ul>
-                  <li><a href="#about">writing</a></li>
+                  <li>
+                    <a href="#about">writing</a>
+                    </li>
                   <li><a href="#about">speaking</a></li>
                   <li><a href="#about">workshop</a></li>
                   <li><a href="#work">playing</a></li>
                   <li><Toggle /> </li>
                 </ul>
+        </nav>
+
+        <nav className="mobile-nav">
+             <ul>
+                  <li><a href="#about">
+                  <h3><TiPencil /></h3>
+                    writing</a></li>
+                  <li><a href="#about">
+                  <h4><TiPencil /></h4>
+                    speaking</a></li>
+                  
+                  <li><a href="#about">
+                  <h4><TiPencil /></h4>
+                    workshop</a></li>
+                  <li><a href="#work">
+                  <h4><TiPencil /></h4>
+                    playing</a></li>
+             </ul>
         </nav>
       </div>
 
