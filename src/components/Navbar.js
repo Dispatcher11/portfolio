53b7873525e45f59668fad5d83ec4109de7ba4e7
belalelgebaly <belalelgebaly4@gmail.com>
2023-01-26 12:51:12 +0200

    navbar strike around hover effect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,12 +17,10 @@ const Navbar = () => {
       dragElastic={.7}>belal</motion.h1>
       <div className="ul">
                 <ul>
-                  <li>
-                    <a href="#about">writing</a>
-                    </li>
-                  <li><a href="#about">speaking</a></li>
-                  <li><a href="#about">workshop</a></li>
-                  <li><a href="#work">playing</a></li>
+                   <li><a href="#" className="menu__link">contact<svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 152.9 43.4" ><path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4"/></svg></a></li>
+                   <li><a href="#" className="menu__link">speaking<svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 152.9 43.4" ><path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4"/></svg></a></li>
+                   <li><a href="#" className="menu__link">workshop<svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 152.9 43.4" ><path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4"/></svg></a></li>
+                   <li><a href="#" className="menu__link">playing<svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 152.9 43.4" ><path d="M151.9,13.6c0,0,3.3-9.5-85-8.3c-97,1.3-58.3,29-58.3,29s9.7,8.1,69.7,8.1c68.3,0,69.3-23.1,69.3-23.1 s1.7-10.5-14.7-18.4"/></svg></a></li>
                 </ul>
                 <Toggle /> 
 </div>
